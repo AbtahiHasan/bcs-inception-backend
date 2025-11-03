8efaa7a48ee8661602209774e93f66de0d22fd39
abtahihasan <abtahihasan7472@gmail.com>
2025-11-03 09:59:30 +0600

    u

--- dist/src/app/middlewares/auth.js
+++ dist/src/app/middlewares/auth.js
@@ -12,24 +12,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const node_1 = require("better-auth/node");
-const http_status_1 = __importDefault(require("http-status"));
-const app_error_1 = __importDefault(require("../errors/app-error"));
-const auth_1 = require("../lib/auth");
 const catch_async_1 = __importDefault(require("../utils/catch-async"));
 const auth = (roles) => {
     return (0, catch_async_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-        var _a;
-        const session = yield auth_1.auth.api.getSession({
-            headers: (0, node_1.fromNodeHeaders)(req.headers),
-        });
-        if (!(session === null || session === void 0 ? void 0 : session.user)) {
-            throw new app_error_1.default(http_status_1.default.NOT_FOUND, "This user is not found !");
-        }
-        if (!roles.includes((_a = session === null || session === void 0 ? void 0 : session.user) === null || _a === void 0 ? void 0 : _a.role)) {
-            throw new app_error_1.default(http_status_1.default.UNAUTHORIZED, "You are not authorized!");
-        }
-        req.user = session === null || session === void 0 ? void 0 : session.user;
+        // const session = await better_auth.api.getSession({
+        //   headers: fromNodeHeaders(req.headers),
+        // });
+        // if (!session?.user) {
+        //   throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
+        // }
+        // if (!roles.includes(session?.user?.role)) {
+        //   throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+        // }
+        // req.user = session?.user;
         next();
     }));
 };
--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -8,19 +8,19 @@ import catch_async from "../utils/catch-async";
 const auth = (roles: ("student" | "admin" | "super_admin")[]) => {
   return catch_async(
     async (req: Request, res: Response, next: NextFunction) => {
-      const session = await better_auth.api.getSession({
-        headers: fromNodeHeaders(req.headers),
-      });
+      // const session = await better_auth.api.getSession({
+      //   headers: fromNodeHeaders(req.headers),
+      // });
 
-      if (!session?.user) {
-        throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
-      }
+      // if (!session?.user) {
+      //   throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
+      // }
 
-      if (!roles.includes(session?.user?.role)) {
-        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
-      }
+      // if (!roles.includes(session?.user?.role)) {
+      //   throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+      // }
 
-      req.user = session?.user;
+      // req.user = session?.user;
       next();
     }
   );
